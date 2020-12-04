a0878a758e1623b217f8d964442d725083f1fa69
Joshua Joshua <joshuapfarina@gmail.com>
2020-12-04 15:15:34 +0000

    almost

--- main.py
+++ main.py
@@ -1,23 +1,36 @@
+def parse_passport(passport):
+  passport = passport.strip().split()
+  passport = [tuple(kv.split(":")) for kv in passport]
+  return dict(passport)
 
-import re
-import json
-required_fields = ('byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid', 'cid')
+def validate_passport(passport, req_fields):
+  s1 = set(req_fields)
+  s2 = set(passport.keys())
+  return s1.issubset(s2)
 
-def get_next_passport(file_stream):
-  passport = ''
-  line = ''
-  while line != '\n':
-    line = f.readline()
-    passport += ' ' + line.strip()
-  passport.strip().split()
 
-f = open("passports_t1")
-    passport = get_next_passport(f)
-print(passport)
-with  open("passports_t1") as f
-    passport = get_next_passport(f)
+required = ('byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid')
+optional =('cid')
 
-  
+
+with  open("passports_t1") as f:
+  chunk = ''
+  n_valid = 0
+  for line in f:
+    if line == '\n':
+      parsed = parse_passport(chunk)
+      valid = validate_passport(parsed, required)
+      print(parsed, valid)
+      n_valid += valid
+      chunk = ''
+    else:
+      chunk += line.strip() + ' '
+    
+print(n_valid)
+
+       
+        
+
   
   
 
